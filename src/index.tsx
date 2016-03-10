3244276d95af417d34c602d463c41fb343a85fc9
Daniel Rosenwasser <drosen@microsoft.com>
2016-03-10 13:23:18 -0800

    Initial commit.

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import * as jQuery from "jquery";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { ApiResponse } from "./reddit";
+import { SubmissionComp } from "./components/RedditSubmission"
+
+function displaySubreddit(subreddit: string) {
+    jQuery.ajax({ url: `https://www.reddit.com/r/${subreddit}.json` }).done((response: ApiResponse) => {
+        let submissions = response.data.children;
+        
+        // Only take submissions that link directly to png and jpg files. 
+        submissions = submissions.filter(({data}) => /(png|jpg)$/.test(data.url));
+        
+        // Create our list of components.
+        let components = submissions
+            .map((value, index) => <SubmissionComp key={index} elementPosition={index} {...value.data} />);
+        
+        ReactDOM.render(<div>{components}</div>, document.getElementById("content"))
+    });
+}
+
+displaySubreddit("aww");
